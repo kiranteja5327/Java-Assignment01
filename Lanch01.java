e0f74692c5cb9bbc8ad2a1220032355c8bb47e12
kiranteja5327 <kiranteja5327@gmail.com>
2022-10-21 15:32:40 +0530

    JavaAssignment01

---
+++ Lanch01.java
@@ -0,0 +1,101 @@
+
+public class Lanch01 {
+
+	public static void main(String[] args)
+	{
+		int n=10;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==n/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || j==n-1 || i==j)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			
+			for(int j=0;j<n;j++)
+			{
+				if(i==0 && j>1 || i==n/2 && j<n/2 && j>1 || i==n-1 && j>1 || j==1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			
+			for(int j=0;j<n;j++)
+			{
+				if(j==1 && i<n-1 ||i==n-1 && j<3*n/4 && j!=1 && j!=0 || j==n-3 && i<n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || i==0 && j<3*n/4 || i==n/2 && j<3*n/4 || j==n-3 && i<n/2 && i!=0 || i-j==n/2 )
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			for(int j=0;j<n;j++)
+			{
+				if(i==0 || i==n-1 || j==0 || j==n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			for(int j=0;j<n;j++)
+			{
+				if(j==1 || j==n-1 || i==j&& i!=0)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+
+			System.out.println();
+		}
+	
+	}
+}
